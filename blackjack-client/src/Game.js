e4392e5a8675475b70bf4267b7ceffdb48a462af
rm704 <rm704@students.waikato.ac.nz>
2024-04-01 21:42:30 +1300

    frontend continuation

--- blackjack-client/src/Game.js
+++ blackjack-client/src/Game.js
@@ -27,6 +27,35 @@ const Game = () => {
     };
 
     const handleStand = () => {
+        //Implement logic for the player to stand
+        axios.post('http://localhost:5000/game/stand',
+            { gameId: gameState._id })
+            .then(response => {
+                setGameState(response.data);
+                checkWinner(response.data.winner);
+            })
+            .catch(error =>
+                console.error("Error standing:", error));
+    };
+
+    const startNewGame = () => {
+        //Implement logic to start a new game
+        setWinnerMessage(''); //Clear the winner message
+        axios.post('http://loaclhost:5000/game/start')
+            .then(response => setGameState(response.data))
+            .catch(error =>
+                console.error('Error starting a new game:', error));
+    };
+
+    const checkWinner = (winner) => {
+        //Display winner message and start a new game
+        setWinnerMessage('Winner: ${winner}');
+        setTimeout(() => {
+            startNewGame();
+        }, 5000);// Automativally start a new game after 5 seconds
+    };
+
+    return (
         
-    }
+    )
 }
